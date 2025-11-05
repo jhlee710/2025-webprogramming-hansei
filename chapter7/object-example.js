b8a38225e228b403fc36aff7f1471385529c9ecf
Jaehwan Lee <ssso00@hansei.ac.kr>
2025-11-05 17:27:54 +0900

    git add . git commit -m git push origin main

---
+++ chapter7/object-example.js
@@ -0,0 +1,25 @@
+let person = {
+    name: "J1",
+    age: 23,
+    hobbies: ['football', 'basketball', 'volleyball'],
+    tall: 180,
+    gender: '',
+    height: 180,
+    sayHello: function() {
+        console.log('안녕하세요, 저는 $(this.name)입니다');
+    }
+}
+
+let dog = {
+    name: '',
+    age: 3,
+    breed: '',
+    bark: funciton() {
+        console.log('멍멍!');
+    }
+}
+
+
+console.log(person.name)
+console.log(person.sayHello());
+console.log(dog.bark());
